9ac40a86a227d28d9675e5878b85a9ec34f49d28
Lundahl <lundahl@Lundahls-MacBook-Air.local>
2019-08-06 19:34:29 +0200

    Improved startup message

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import config
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+    print('Connected to {} (with result code {})'.format(config.mqtt_server, str(rc)))
     client.subscribe(config.topic_sub)
 
 # The callback for when a PUBLISH message is received from the server.
