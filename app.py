da7f85755abfb5455dcb5812a324f7e337a6c20c
Lundahl <lundahl@Lundahls-MacBook-Air.local>
2019-08-07 19:24:03 +0200

    Making app use the Sensor class

--- app.py
+++ app.py
@@ -3,6 +3,14 @@ import json
 import slack
 import config
 from datetime import datetime
+import validator
+from sensor import Sensor
+
+sensor_checker = validator.Checker().all()
+sensor_checker.add_rule(lambda x: x.temperature > 15, 'Temperature is to low.')
+sensor_checker.add_rule(lambda x: x.temperature < 30, 'Temperature is to high.')
+sensor_checker.add_rule(lambda x: x.humidity > 30, 'Humidity is to low.')
+sensor_checker.add_rule(lambda x: x.humidity < 70, 'Humidity is to high.')
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -13,20 +21,20 @@ def on_connect(client, userdata, flags, rc):
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    if msg.topic == config.topic_sub:
-        vals = json.loads(msg.payload)
-        if vals['temperature'] < 18 or vals['temperature'] > 30:
-            #print('Temperature is to low or high!')
-            slack.post('Temperature is to low or high! Temperature is {} C and humidity is {} %.'.format(vals['temperature'], vals['humidity']))
-            
-        if vals['humidity'] < 30 or vals['humidity'] > 70:
-            #print('Humidity is to low or high!')
-            slack.post('Humidity is to low or high! Temperature is {} C and humidity is {} %.'.format(vals['temperature'], vals['humidity']))
     ts = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     print('{} {}'.format(ts, msg.payload))
 
+    if msg.topic == config.topic_sub:
+        vals = json.loads(msg.payload)
+        sensor = Sensor.from_json(vals)
+        if not sensor_checker.validate(sensor):
+            alarm = sensor_checker.evaluate(sensor)
+            message = 'Warning {}! {} Temperature {} C, Humidity {} %'.format(sensor.name, alarm, sensor.temperature, sensor.humidity)
+            print(message)
+            slack.post(message)
+
 if __name__ == '__main__':
-    client = mqtt.Client(protocol=mqtt.MQTTv31)
+    client = mqtt.Client()
     client.on_connect = on_connect
     client.on_message = on_message
     client.connect(config.mqtt_server, 1883, 60)
