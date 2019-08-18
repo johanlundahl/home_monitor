497a77ea91393b799d4e4c7fbd9d54f15456f867
Lundahl <lundahl@Lundahls-MacBook-Air.local>
2019-08-18 09:03:29 +0200

    extracted sensor decoder to its own class

--- sensor.py
+++ sensor.py
@@ -7,22 +7,25 @@ class Sensor:
         self.humidity = humidity
 
     def to_json(self):
-        return json.dumps(self, cls=SensorSerializer)
+        return json.dumps(self, cls=SensorEncoder)
 
     @classmethod
     def from_json(cls, dct):
-        return json.loads(dct, object_hook=SensorSerializer.decode)
+        return json.loads(dct, object_hook=SensorDecoder.decode)
 
     def __str__(self):
         return 'Sensor({}, {}, {})'.format(self.name, self.temperature, self.humidity)
 
-class SensorSerializer(json.JSONEncoder):
+
+class SensorEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, Sensor):
             return {'type': 'Sensor', 'name': obj.name, 'temperature': obj.temperature, 'humidity': obj.humidity}
         else:
             return super().default(obj)
 
-    def decode(dct):
-        if 'type' in dct and dct['type'] == 'Sensor':
+class SensorDecoder():
+    @classmethod
+    def decode(cls, dct):
+        if 'temperature' in dct and 'humidity' in dct:
             return Sensor(dct['name'], dct['temperature'], dct['humidity'])
