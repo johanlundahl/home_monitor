f20d7cd40e3997b205a137418ccd1086192c09b7
Lundahl <lundahl@Lundahls-MacBook-Air.local>
2019-08-18 09:01:58 +0200

    logging last received sensor reading and checks elapsed time since last reading on a set interval

--- app.py
+++ app.py
@@ -5,12 +5,16 @@ import config
 from datetime import datetime
 from pytils import validator
 from sensor import Sensor
+import time
 
 sensor_checker = validator.Checker().all()
 sensor_checker.add_rule(lambda x: x.temperature > 15, 'Temperature is to low.')
 sensor_checker.add_rule(lambda x: x.temperature < 30, 'Temperature is to high.')
 sensor_checker.add_rule(lambda x: x.humidity > 30, 'Humidity is to low.')
 sensor_checker.add_rule(lambda x: x.humidity < 70, 'Humidity is to high.')
+
+counter = 0
+sensor_readings = {}
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -24,18 +28,39 @@ def on_message(client, userdata, msg):
     ts = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     print('{} {}'.format(ts, msg.payload))
 
+    global counter
+    counter += 1
+
     if msg.topic == config.topic_sub:
-        vals = json.loads(msg.payload)
-        sensor = Sensor.from_json(vals)
+        sensor = Sensor.from_json(msg.payload)
+        
+        global sensor_readings
+        sensor_readings[sensor.name] = (datetime.now(), sensor)
         if not sensor_checker.validate(sensor):
             alarm = sensor_checker.evaluate(sensor)
             message = 'Warning {}! {} Temperature {} C, Humidity {} %'.format(sensor.name, alarm, sensor.temperature, sensor.humidity)
             print(message)
             slack.post(message)
 
-if __name__ == '__main__':
+def run():
     client = mqtt.Client()
     client.on_connect = on_connect
     client.on_message = on_message
     client.connect(config.mqtt_server, 1883, 60)
-    client.loop_forever()
+    client.loop_start()
+
+    while True:
+        time.sleep(10)
+        print('Sensor life check...')
+        for name in sensor_readings:
+            dt, sensor = sensor_readings[name]
+            print((datetime.now()-dt).seconds, 'seconds since last reading')
+            # check and alarm if sensor reading is older than 5 minutes
+
+    client.disconnect()
+    client.loop_stop()
+
+
+if __name__ == '__main__':
+    run()
+    
