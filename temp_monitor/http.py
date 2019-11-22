3609be963dd2c4a263a82a82fcc19fb2b05abdbf
Lundahl <lundahl@Lundahls-MacBook-Air.local>
2019-11-22 20:53:17 +0100

    return status code instead of printing it

--- temp_monitor/http.py
+++ temp_monitor/http.py
@@ -2,8 +2,8 @@ import requests
 from temp_monitor import config
 import json
 
-def post(obj):
+def post(obj, name):
 	print(obj)
 	headers = {'content-type': 'application/json'}
 	response = requests.post(config.save_sensor_url, data = obj, headers = headers)
-	print(response.status_code)
+	return response.status_code
