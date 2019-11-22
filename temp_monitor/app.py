e08ea1abfeafd012dc5934efb34804456b4a1aca
Lundahl <lundahl@Lundahls-MacBook-Air.local>
2019-11-22 20:52:23 +0100

    refactored to more methods

--- temp_monitor/app.py
+++ temp_monitor/app.py
@@ -12,7 +12,6 @@ sensor_checker.add_rule(lambda x: x.temperature < 30, 'Temperature is to high.')
 sensor_checker.add_rule(lambda x: x.humidity > 30, 'Humidity is to low.')
 sensor_checker.add_rule(lambda x: x.humidity < 70, 'Humidity is to high.')
 
-counter = 0
 sensor_readings = {}
 
 # The callback for when the client receives a CONNACK response from the server.
@@ -27,27 +26,35 @@ def on_message(client, userdata, msg):
     ts = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     print('{} {}'.format(ts, msg.payload))
 
-    global counter
-    counter += 1
-
     if msg.topic == config.topic_sub:
         sensor = Sensor.from_json(msg.payload)
-        http.post(sensor.to_json())
+        status_code = http.post(sensor.to_json(), sensor.name)
         
         global sensor_readings
         sensor_readings[sensor.name] = (datetime.now(), sensor)
         if not sensor_checker.validate(sensor):
             alarm = sensor_checker.evaluate(sensor)
-            message = 'Warning {}! {} Temperature {} C, Humidity {} %'.format(sensor.name, alarm, sensor.temperature, sensor.humidity)
-            print(message)
-            slack.post(message)
+            notify(alarm, sensor)        
 
-def run():
+def notify(alarm, sensor):
+    message = 'Warning {}! {} Temperature {} C, Humidity {} %'.format(sensor.name, alarm, sensor.temperature, sensor.humidity)
+    print(message)
+    slack.post(message)
+
+def start_client():
     client = mqtt.Client()
     client.on_connect = on_connect
     client.on_message = on_message
     client.connect(config.mqtt_server, 1883, 60)
     client.loop_start()
+    return client
+
+def stop_client(client):
+    client.disconnect()
+    client.loop_stop()
+
+def run():
+    client = start_client()
 
     while True:
         time.sleep(10)
@@ -56,10 +63,7 @@ def run():
             if (datetime.now()-dt).seconds > 60*5:
                 print((datetime.now()-dt).seconds, 'seconds since last reading')
             
-    client.disconnect()
-    client.loop_stop()
-
+    stop_client(client)
 
 if __name__ == '__main__':
     run()
-    
