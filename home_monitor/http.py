29544518c99af2aafac54c729293a6946120d590
Lundahl <lundahl@Lundahls-MacBook-Air.local>
2019-11-24 18:53:18 +0100

    files moved to pytils

--- home_monitor/http.py
+++
@@ -1,9 +0,0 @@
-import requests
-from home_monitor import config
-import json
-
-def post(obj, name):
-	print(obj)
-	headers = {'content-type': 'application/json'}
-	response = requests.post(config.save_sensor_url, data = obj, headers = headers)
-	return response.status_code
--- home_monitor/slack.py
+++
@@ -1,9 +0,0 @@
-import requests
-from home_monitor import config
-
-def post(message, image_url = None):
-    payload = {"text": "{}".format(message)}
-    if image_url is not None:
-        payload['attachments'] = [{'fallback':'Overview of this weeks business.', 'image_url': image_url}]
-    r = requests.post(url = config.slack_webhook_url, json = payload)
-    return r.status_code, r.text
