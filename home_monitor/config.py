27aa735f2622eb8592fe65b2905480a3891e7e14
Lundahl <lundahl@Lundahls-MacBook-Air.local>
2019-12-13 18:55:04 +0100

    adding config file

---
+++ home_monitor/config.py
@@ -0,0 +1,4 @@
+mqtt_server = '31.208.165.29'
+topic_sub = 'sensors'
+slack_webhook_url = 'https://hooks.slack.com/services/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'
+save_sensor_url = 'http://localhost:5000/api/sensors'
