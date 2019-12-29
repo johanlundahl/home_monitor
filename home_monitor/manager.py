6e6f47d56aef25787e88aeaca204da8dffd7c48b
Lundahl <lundahl@Lundahls-MacBook-Air.local>
2019-12-29 21:00:53 +0100

    rearranged rules

--- home_monitor/manager.py
+++ home_monitor/manager.py
@@ -6,10 +6,11 @@ from pytils import validator, slack, http, logger
 class SensorManager:
     
     sensor_checker = validator.Checker().all()
-    sensor_checker.add_rule(lambda x: x.temperature > 15 and x.name == 'basement', 'Temperature is to low.')
-    sensor_checker.add_rule(lambda x: x.temperature < 30 and x.name == 'basement', 'Temperature is to high.')
-    sensor_checker.add_rule(lambda x: x.humidity > 30 and x.name == 'basement', 'Humidity is to low.')
-    sensor_checker.add_rule(lambda x: x.humidity < 70 and x.name == 'basement', 'Humidity is to high.')
+    sensor_checker.add_rule(lambda x: x.temperature < 15 and x.name == 'basement', 'Temperature is to low.')
+    sensor_checker.add_rule(lambda x: x.temperature > 30 and x.name == 'basement', 'Temperature is to high.')
+    sensor_checker.add_rule(lambda x: x.humidity < 30 and x.name == 'basement', 'Humidity is to low.')
+    sensor_checker.add_rule(lambda x: x.humidity > 70 and x.name == 'basement', 'Humidity is to high.')
+    sensor_checker.add_rule(lambda x: x.temperature <= 0 and x.name == 'outdoor', 'Its cold outside! Save paint and batteries :)')
 
     def __init__(self, lifetime=24):
         self._sensors = {}
@@ -25,7 +26,7 @@ class SensorManager:
         self.check(sensor)
 
     def check(self, sensor):
-        if not self.sensor_checker.validate(sensor.reading):
+        if self.sensor_checker.validate(sensor.reading):
             if not sensor.alarm_state:
                 alarm = self.sensor_checker.evaluate(sensor.reading)
                 self.notify(alarm, sensor.reading)
