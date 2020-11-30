f2f960fbd4cdbbf5b1a6b6266bfa386d80e8a6c7
Johan Lundahl <johan.t.lundahl@gmail.com>
2020-11-30 18:32:14 +0100

    adjusted to chain of responsibility commands

--- home_monitor/manager.py
+++ home_monitor/manager.py
@@ -1,43 +1,19 @@
 import datetime
-from home_monitor.model.sensor import Sensor, Reading
-from pytils import validator, slack, http, log
+from home_monitor.handlers import Persist, Validator 
+from home_monitor.models import Reading
 
 
 class SensorManager:
     
-    sensor_checker = validator.Checker().all()
-    sensor_checker.add_rule(lambda x: x.temperature < 15 and x.name == 'basement', 'Temperature is to low.')
-    sensor_checker.add_rule(lambda x: x.temperature > 30 and x.name == 'basement', 'Temperature is to high.')
-    sensor_checker.add_rule(lambda x: x.humidity < 30 and x.name == 'basement', 'Humidity is to low.')
-    sensor_checker.add_rule(lambda x: x.humidity > 70 and x.name == 'basement', 'Humidity is to high.')
-    sensor_checker.add_rule(lambda x: x.temperature <= 0 and x.name == 'outdoor', 'Its cold outside! Save paint and batteries :)')
-
     def __init__(self, sensor_url, slack_url, lifetime=24):
-        self._sensors = {}
         self.save_sensor_url = sensor_url
         self.slack_webhook_url = slack_url
 
-    def update(self, reading):
-        if reading.name in self._sensors:
-            self._sensors[reading.name].reading = reading
-        else:
-            self._sensors[reading.name] = Sensor(reading)
+        # set up chain of responsibity commands for handling incoming sensors
+        persist = Persist(url=sensor_url)
+        validator = Validator(persist)
+        self.first_command = validator
 
-        sensor = self._sensors[reading.name]
-        status_code = http.post_json(self.save_sensor_url, reading.to_json())
-        self.check(sensor)
-
-    def check(self, sensor):
-        if self.sensor_checker.validate(sensor.reading):
-            if not sensor.alarm_state:
-                alarm = self.sensor_checker.evaluate(sensor.reading)
-                self.notify(alarm, sensor.reading)
-                log.warning(alarm)
-            sensor.alarm_state = True
-        elif sensor.alarm_state:
-                sensor.alarm_state = False
-                # Notify back to normal?
-
-    def notify(self, alarm, sensor):
-        message = 'Warning {}! {} Temperature {} C, Humidity {} %'.format(sensor.name, alarm, sensor.temperature, sensor.humidity)
-        slack.post(self.slack_webhook_url, message)
+    def delegate(self, reading: Reading):
+        self.first_command.handle(reading)
+   
