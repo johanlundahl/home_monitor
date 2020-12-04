f064777475794f226c440f5faf903c45fc870a94
Johan Lundahl <johan.t.lundahl@gmail.com>
2020-12-04 21:18:28 +0100

    removed validation handler and added alarm handler

--- home_monitor/handlers.py
+++ home_monitor/handlers.py
@@ -1,6 +1,7 @@
 import abc
+from enum import Enum
 import logging
-from pytils import http
+from pytils import http, slack
 from home_monitor.models import Sensor, Reading
 
 
@@ -9,35 +10,18 @@ class Handler(metaclass=abc.ABCMeta):
     def __init__(self, next_command=None):
         self._next = next_command
 
-    def handle(self, reading):
-        self.process(reading)
+    def handle(self, sensor):
+        self.process(sensor)
         if self.proceed:
-            self._next.handle(reading)
+            self._next.handle(sensor)
 
     @property
     def proceed(self):
         return self._next != None
 
     @abc.abstractmethod
-    def process(self, reading):
+    def process(self, sensor):
         pass
-
-
-class ValidateHandler(Handler):
-
-    def __init__(self, next_command=None):
-        self._next = next_command
-        self._sensors = {}
-
-    def process(self, reading: Reading):
-        logging.info('{} processing of {}'.format(type(self).__name__, str(reading)))
-        sensor = self.get_sensor(reading)
-        sensor.reading = reading
-    
-    def get_sensor(self, reading: Reading):
-        if reading.name not in self._sensors:
-            self._sensors[reading.name] = Sensor.create(reading)
-        return self._sensors[reading.name]            
 
 
 class PersistHandler(Handler):
@@ -46,24 +30,28 @@ class PersistHandler(Handler):
         self._next = next_command
         self._url = url
 
-    def process(self, reading):
-        logging.info('{} processing of {}'.format(type(self).__name__, str(reading)))
-        status_code, data = http.post_json(self._url, reading.to_json())
+    def process(self, sensor):
+        logging.info('{} processing of {}'.format(type(self).__name__, str(sensor)))
+        status_code, data = http.post_json(self._url, sensor.reading.to_json())
         if status_code != 200:
-            logging.error('Status code {} when POST\'ing {}'.format(status_code, reading.to_json()))
+            logging.error('Status code {} when POST\'ing {}'.format(status_code, sensor.reading.to_json()))
     
 
 class AlarmHandler(Handler):
 
-    def __init__(self, next_command=None):
+    def __init__(self, next_command=None, slack_webhook_url=''):
         self._next = next_command
-        self._alarmed = {}
+        self._slack_webhook_url = slack_webhook_url
+        
+    def process(self, sensor):
+        logging.info('{} processing of {}'.format(type(self).__name__, str(sensor)))
+        if sensor.triggered:
+            self.raise_alarm(sensor)
+            sensor.alarm_raised()
 
-    def process(self, request):
-        request.history.append('notify if alarm')
-        # check if alarm and if have been notified before
-        # do not alarm if sensor is not in dict
+    def raise_alarm(self, sensor):
+        message = 'Warning {}! Temperature {} C, Humidity {} %'.format(sensor.reading.name, 
+            sensor.reading.temperature, 
+            sensor.reading.humidity)
+        slack.post(self._slack_webhook_url, message)
 
-    #def notify(self, alarm, sensor):
-        #    message = 'Warning {}! {} Temperature {} C, Humidity {} %'.format(sensor.name, alarm, sensor.temperature, sensor.humidity)
-        #    slack.post(self.slack_webhook_url, message)
