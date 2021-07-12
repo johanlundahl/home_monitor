92baad20fccaeefad574b5c570343e91d2c7cb35
Johan Lundahl <johan.t.lundahl@gmail.com>
2021-07-12 14:21:06 +0200

    corrected lint errors

--- home_monitor/alarms.py
+++ home_monitor/alarms.py
@@ -6,11 +6,21 @@ class State(metaclass=abc.ABCMeta):
 
     def __init__(self):
         self.alarm_checker = validator.Checker().any()
-        self.alarm_checker.add_rule(lambda x: x.temperature < 15 and x.name == 'basement', 'Temperature is to low.')
-        self.alarm_checker.add_rule(lambda x: x.temperature > 30 and x.name == 'basement', 'Temperature is to high.')
-        self.alarm_checker.add_rule(lambda x: x.humidity < 30 and x.name == 'basement', 'Humidity is to low.')
-        self.alarm_checker.add_rule(lambda x: x.humidity > 70 and x.name == 'basement', 'Humidity is to high.')
-        self.alarm_checker.add_rule(lambda x: x.temperature <= 0 and x.name == 'outdoor', 'Its cold outside! Save paint and batteries :)')
+        self.alarm_checker.add_rule(lambda x: x.temperature < 15 and
+                                    x.name == 'basement',
+                                    'Temperature is to low.')
+        self.alarm_checker.add_rule(lambda x: x.temperature > 30 and
+                                    x.name == 'basement',
+                                    'Temperature is to high.')
+        self.alarm_checker.add_rule(lambda x: x.humidity < 30 and
+                                    x.name == 'basement',
+                                    'Humidity is to low.')
+        self.alarm_checker.add_rule(lambda x: x.humidity > 70 and
+                                    x.name == 'basement',
+                                    'Humidity is to high.')
+        self.alarm_checker.add_rule(lambda x: x.temperature <= 0 and
+                                    x.name == 'outdoor',
+                                    'Its cold! Save paint and batteries :)')
 
     @abc.abstractmethod
     def on_event(self, reading):
